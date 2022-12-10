a709288046c5a8c9ae3a8ea4b9eeddfc51f15f8e
Casey Dunn <casey.dunn@yale.edu>
2022-12-10 07:17:25 -0500

    bug fixes

--- rarefaction_dts/batch_snake.sh
+++ rarefaction_dts/batch_snake.sh
@@ -6,14 +6,27 @@
 #SBATCH --partition=pi_dunn
 #SBATCH --nodes=1                    # number of cores and nodes
 #SBATCH --cpus-per-task=18           # number of cores
-#SBATCH --mem-per-cpu=4G             # shared memory, scaling with CPU request
+#SBATCH --mem-per-cpu=10G             # shared memory, scaling with CPU request
 
-export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
+# Set up paths
+cwd=$(pwd)
+echo $cwd
+base=$(basename $cwd)
+echo $base
+scratch_dir="/gpfs/ysm/scratch60/dunn/cwd7/$base"
+echo "Using this scratch dir for output: "
+echo $scratch_dir
+mkdir -p $scratch_dir
 
+module purge # Unload any existing modules that might conflict
 module load Jellyfish
-# module load snakemake # This one is an old version
 module load miniconda
-#mamba activate snakemake
 conda activate snakemake
-snakemake --cores $SLURM_CPUS_PER_TASK --snakefile GGSR
+
+snakemake \
+  --cores $SLURM_CPUS_PER_TASK \
+  --directory $scratch_dir \
+  --configfile config.yaml \
+  --snakefile GGSR
+
 # snakemake --unlock --cores $SLURM_CPUS_PER_TASK
