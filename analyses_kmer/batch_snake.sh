1e7ce5c2527937d0e922e2edd36a2ba46807fd88
Casey Dunn <casey.dunn@yale.edu>
2023-07-22 09:40:24 -0400

    added trimmomatic

--- analyses_kmer/batch_snake.sh
+++ analyses_kmer/batch_snake.sh
@@ -3,14 +3,14 @@
 #SBATCH --job-name=snake
 #SBATCH --output=snake.txt
 #SBATCH --time=1-00:00:00
-#SBATCH --partition=general
+#SBATCH --partition=pi_dunn
 #SBATCH --nodes=1                    # number of cores and nodes
-#SBATCH --cpus-per-task=8           # number of cores
-#SBATCH --mem-per-cpu=10G             # shared memory, scaling with CPU request
+#SBATCH --cpus-per-task=16           # number of cores
+#SBATCH --mem-per-cpu=10G            # shared memory, scaling with CPU request
 
 export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
 
-module load Jellyfish
+# module load Jellyfish
 # module load snakemake # This one is an old version
 module load miniconda
 #mamba activate snakemake
