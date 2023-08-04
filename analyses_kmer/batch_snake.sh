2ea3523a43b856bbbba4106b62a308ed4a9a9225
Casey Dunn <casey.dunn@yale.edu>
2023-08-04 09:36:54 -0400

    mapping

--- analyses_kmer/batch_snake.sh
+++ analyses_kmer/batch_snake.sh
@@ -8,6 +8,9 @@
 #SBATCH --cpus-per-task=16           # number of cores
 #SBATCH --mem-per-cpu=10G            # shared memory, scaling with CPU request
 
+module load BEDTools
+module load SAMtools
 module load miniconda
+module load BWA
 conda activate snakemake
 snakemake --cores $SLURM_CPUS_PER_TASK
