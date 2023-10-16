6ecf0fe3619bab37385c1ff209a7f978ed29156a
Casey Dunn <casey.dunn@yale.edu>
2023-10-16 16:38:43 -0400

    minor changes for run in new dir

--- analyses_sharkmer/batch_snake.sh
+++ analyses_sharkmer/batch_snake.sh
@@ -2,11 +2,11 @@
 
 #SBATCH --job-name=snake
 #SBATCH --output=snake.txt
-#SBATCH --time=1-00:00:00
+#SBATCH --time=4-00:00:00
 #SBATCH --partition=ycga_bigmem
 #SBATCH --nodes=1                    # number of cores and nodes
 #SBATCH --cpus-per-task=16           # number of cores
-#SBATCH --mem-per-cpu=60G            # shared memory, scaling with CPU request
+#SBATCH --mem-per-cpu=120G            # shared memory, scaling with CPU request
 
 # For sharkmer, use --partition=ycga_bigmem    --cpus-per-task=16    --mem-per-cpu=60G
 # For most other steups, use --partition=pi_dunn   --cpus-per-task=16   --mem-per-cpu=4G
